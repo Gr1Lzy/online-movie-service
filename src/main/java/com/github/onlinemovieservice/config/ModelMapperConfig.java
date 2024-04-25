5f8ee7d155ae6b40f720dcc343a68b462aff0c59
Andrii Kolomoiets <kolomoets02@gmail.com>
2024-04-25 15:02:15 +0300

    add ModelMapper configuration

--- src/main/java/com/github/onlinemovieservice/config/ModelMapperConfig.java
+++ src/main/java/com/github/onlinemovieservice/config/ModelMapperConfig.java
@@ -1,2 +1,13 @@
-package com.github.onlinemovieservice.config;public class ModelMapperConfig {
+package com.github.onlinemovieservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
