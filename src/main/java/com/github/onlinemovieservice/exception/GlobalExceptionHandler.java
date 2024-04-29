6d2090f7b149da2c8406cebd9ebf1958919e5083
Andrii Kolomoiets <kolomoets02@gmail.com>
2024-04-29 16:32:08 +0300

    change GlobalHandler

--- src/main/java/com/github/onlinemovieservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/onlinemovieservice/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,30 +15,33 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    protected ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e) {
-        log.warn("IllegalArgumentException thrown: {}", e.getMessage());
+    @ExceptionHandler({IllegalArgumentException.class, HttpMessageNotReadableException.class})
+    protected ResponseEntity<Object> handleBadRequestException(Exception e) {
+        log.warn("Bad request exception thrown: {}", e.getMessage());
         return buildErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    protected ResponseEntity<Object> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        log.warn("HttpMessageNotReadableException thrown: {}", e.getMessage());
-        return buildErrorResponse(e.getMessage());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.warn("MethodArgumentNotValidException thrown: {}", e.getMessage());
+        StringBuilder errorMessage = new StringBuilder();
+        e.getBindingResult().getFieldErrors().forEach(fieldError ->
+                errorMessage.append(fieldError.getDefaultMessage()).append(". "));
+        return buildErrorResponse(errorMessage.toString());
     }
 
     private static ResponseEntity<Object> buildErrorResponse(String message) {
-        ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), message);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(response);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(), message));
     }
 
     @Getter
     @AllArgsConstructor
     @JsonInclude(JsonInclude.Include.NON_NULL)
     static class ErrorResponse {
-        private int status;
-        private String error;
-        private String message;
+        private final int status;
+        private final String error;
+        private final String message;
     }
 }
 
