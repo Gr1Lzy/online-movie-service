d118d6971c034588d1a98d831d0737158b93c4e8
Andrii Kolomoiets <kolomoets02@gmail.com>
2024-07-03 01:56:59 +0300

    changes

--- src/main/java/com/github/onlinemovieservice/config/WebConfig.java
+++ src/main/java/com/github/onlinemovieservice/config/WebConfig.java
@@ -14,9 +14,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3050")
                 .allowedOrigins(frontendUri)
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
