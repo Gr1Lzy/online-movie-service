dbed80b70222e414ce77d29f1f4cb7f188d7c5d7
Andrii Kolomoiets <kolomoets02@gmail.com>
2024-07-02 20:35:40 +0300

    add frontend Uri

--- src/main/java/com/github/onlinemovieservice/config/WebConfig.java
+++ src/main/java/com/github/onlinemovieservice/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.github.onlinemovieservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.uri}")
+    private String frontendUri;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // Configure CORS globally here
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3050")
+                .allowedOrigins(frontendUri)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
